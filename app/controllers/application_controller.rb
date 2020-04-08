d6785d72f4eb069a32a5044cb73b779ec53cb7a4
shangyukyo <shangyukyo>
2020-04-08 15:24:38 +0800

    refine

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :set_current_locale
+  
+  def set_current_locale
+    I18n.locale = cookies[:locale] || I18n.default_locale    
+  end  
 end
